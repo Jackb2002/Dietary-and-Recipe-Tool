dfef1a41085b45f62fd9474c0efeb218d192f6af
Jack Bassilious <jackbassilious@gmail.com>
2023-11-05 17:51:29 +0000

    Added Info Presenter program for easy testing, added exception handling and timeout to request

---
+++ InfoPresenter/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string html = SupermarketInfo.WebTools.GetPageHTML(@"https://www.tesco.com/groceries/en-GB/shop/fresh-food/fresh-fruit/bananas");
+        Console.WriteLine(html);
+        Console.ReadKey();
+    }
+}
