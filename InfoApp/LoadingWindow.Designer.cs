e14154f7998756916337b45e15251d2a72eed2e6
Jack Bassilious <jackbassilious@outlook.com>
2024-02-15 22:41:35 +0000

    Added progress bar and background worker for loading

---
+++ InfoApp/LoadingWindow.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace InfoApp
+{
+    partial class LoadingWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadingLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // loadingLabel
+            // 
+            this.loadingLabel.AutoSize = true;
+            this.loadingLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadingLabel.Location = new System.Drawing.Point(47, 9);
+            this.loadingLabel.Name = "loadingLabel";
+            this.loadingLabel.Size = new System.Drawing.Size(462, 122);
+            this.loadingLabel.TabIndex = 0;
+            this.loadingLabel.Text = "Please wait while\r\n the content loads...";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 188);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(530, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // LoadingWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 223);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.loadingLabel);
+            this.Name = "LoadingWindow";
+            this.Text = "LoadingWindow";
+            this.Shown += new System.EventHandler(this.LoadingWindow_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loadingLabel;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
---
+++ InfoApp/LoadingWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace InfoApp
+{
+    public partial class LoadingWindow : Form
+    {
+        public LoadingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingWindow_Shown(object sender, EventArgs e)
+        {
+            BackgroundWorker fileCheckerUpdater = new BackgroundWorker();
+            fileCheckerUpdater.DoWork += worker_DoWork;
+            fileCheckerUpdater.WorkerReportsProgress = true;
+            fileCheckerUpdater.ProgressChanged += worker_ProgressChanged;
+            fileCheckerUpdater.RunWorkerCompleted += worker_RunWorkerCompleted;
+            fileCheckerUpdater.RunWorkerAsync();
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Error != null)
+            {
+                Debug.WriteLine("Error: " + e.Error.Message);
+            }
+            else if(e.Cancelled)
+            {
+                Debug.WriteLine("Worker was cancelled");
+            }
+            else
+            {
+                Debug.WriteLine("Worker completed, downloaded and checked all supermarket info");
+            }   
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Code for checking and then downloading the HTML for each page
+        }
+    }
+}
