1b13178c2f7c09b18fb6cffa1a4a51467c27169a
Jack Bassilious <jackbassilious@outlook.com>
2024-02-15 22:31:43 +0000

    Swapped to GUI for info app

--- InfoPresenter/Program.cs
+++ InfoPresenter/Program.cs
@@ -1,9 +1,14 @@
-﻿internal class Program
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         string html = SupermarketInfo.PageDownloader.DownloadPage(@"https://www.tesco.com/groceries/en-GB/shop/fresh-food/fresh-fruit/bananas");
-        Console.WriteLine(html);
+        //Copy to clipboard
+        Clipboard.SetText(html);
         Console.ReadKey();
     }
 }
