1b4eb972b0d1f1f5a030f995cf84b513e08ca663
jackb <Jackbassilious@gmail.com>
2024-02-23 23:46:50 +0000

    Updated model constructors

--- WinFormsInfoApp/Models/Recipe.cs
+++ WinFormsInfoApp/Models/Recipe.cs
@@ -15,5 +15,16 @@ namespace WinFormsInfoApp.Models
         public string WebsiteURL { get; set; }
         public List<Ingredient>? Ingredients { get; set; }
         public List<Diet>? Diets { get; set; }
+
+        public Recipe(int recipeId, string name, string description, string imageURL, string websiteURL, List<Ingredient> ingredients, List<Diet> diets)
+        {
+            RecipeId = recipeId;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            ImageURL = imageURL ?? throw new ArgumentNullException(nameof(imageURL));
+            WebsiteURL = websiteURL ?? throw new ArgumentNullException(nameof(websiteURL));
+            Ingredients = ingredients ?? throw new ArgumentNullException(nameof(ingredients));
+            Diets = diets ?? throw new ArgumentNullException(nameof(diets));
+        }
     }
 }
