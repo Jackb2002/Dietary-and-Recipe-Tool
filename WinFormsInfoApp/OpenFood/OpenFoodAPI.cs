38e5f5ffcc6005def3ed232494948410d9efcd7c
Jack Bassilious <jackbassilious@outlook.com>
2024-03-10 00:25:41 +0000

    Changed ingredient search method

--- WinFormsInfoApp/OpenFood/OpenFoodAPI.cs
+++ WinFormsInfoApp/OpenFood/OpenFoodAPI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +20,9 @@ namespace WinFormsInfoApp.OpenFood
         /// </summary>
         /// 
         public string AccessString =>
-            "https://world.openfoodfacts.net/api/";
+            @"https://world.openfoodfacts.net/api/";
         private string customUserAgent = "Foodapp/Testing Nomail";
+        private string SearchString => AccessString + @"v2/search?q=";
 
         public List<Ingredient> GetAllIngredients(string name)
         {
@@ -28,7 +31,44 @@ namespace WinFormsInfoApp.OpenFood
 
         public Ingredient GetFirstIngredient(string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                HttpResponseMessage response;
+                using (HttpClient client = new HttpClient())
+                {
+                    string request = SearchString + name;
+                    client.DefaultRequestHeaders.Add("User-Agent", customUserAgent);
+                    response = client.GetAsync(request).Result;
+                }
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonRes = response.Content.ReadAsStringAsync().Result;
+                    JObject json = JObject.Parse(jsonRes);
+
+                    // Deserialize the JSON into objects
+                    var products = JsonConvert.DeserializeObject<ProductBaseItem>(jsonResponse).Products;
+
+                    // Search for the product by name
+                    foreach (var product in products)
+                    {
+                        if (product.ProductName == productName)
+                        {
+                            Console.WriteLine($"Product Name: {product.ProductName}");
+                            Console.WriteLine($"Weight: {product.ProductQuantity} {product.ProductQuantityUnit}");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
         }
 
         public bool TestConnection()
