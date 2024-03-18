825c1b7d4559df31e2a8c4287e403e40fa4c66e7
Jack Bassilious <jackbassilious@gmail.com>
2024-03-18 12:08:26 +0000

    Removed redundent settings

--- WinFormsInfoApp/GlobalSettings.cs
+++ WinFormsInfoApp/GlobalSettings.cs
@@ -2,7 +2,6 @@
 {
     internal static class GlobalSettings
     {
-        internal static readonly string SupermarketURLsFile = @"Pages.dat";
         internal static readonly string LocalDatabaseFile = "Food_db.db";
         internal static string FullPathDb = LocalDatabaseFile;
     }
