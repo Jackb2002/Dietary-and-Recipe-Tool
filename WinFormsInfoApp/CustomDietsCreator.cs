3f7efd08f1473fd81a16ff2b1785405b741365eb
jackb <Jackbassilious@gmail.com>
2024-03-27 13:38:59 +0000

    Added new forms for diet functions

---
+++ WinFormsInfoApp/CustomDietsCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsInfoApp
+{
+    public partial class CustomDietsCreator : Form
+    {
+        public CustomDietsCreator()
+        {
+            InitializeComponent();
+        }
+    }
+}
